0b2a555f9762a6116245f33609d5862fa7186783
Rob-Dev007 <robdev0695@gmail.com>
2026-02-11 13:03:38 -0500

    feat(certificates): agrega configuración inicial de certificados con imágenes

---
+++ Frontend/src/services/data/getCertificates.js
@@ -0,0 +1,12 @@
+import CursoJS  from "../../assets/certificados/curso-js.jpg";
+import CursoJSAlgorithms from "../../assets/certificados/certificado-algorithms-and-data-structure.png";
+import CursoResponsiveWeb from "../../assets/certificados/certificado-responsive-design.png";
+import CursoDevJs from "../../assets/certificados/certificado-dev-js.png"
+
+export const getCertificates = ()=> [
+  { id: 1, src: CursoJS , title: "Curso Javascript Moderno" },
+  { id: 2, src: CursoJSAlgorithms, title: "Curso Javascript algorithms and data structures" },
+  { id: 3, src: CursoResponsiveWeb, title: "Curso responsive web design" },
+  { id: 4, src: CursoDevJs, title: "Curso Dev Js" },
+];
+
